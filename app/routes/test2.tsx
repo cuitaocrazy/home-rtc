807ff2cfec7b6fc69c53178f59346652a24a06c5
cuitao <ctllf@gmail.com>
2022-10-27 23:08:59 +0800

    add select

--- app/routes/test2.tsx
+++ app/routes/test2.tsx
@@ -1,22 +1,100 @@
-import { useSyncExternalStore } from 'react'
+import type { FC } from 'react'
+import { useEffect } from 'react'
+import { useCallback } from 'react'
+import { useRef } from 'react'
+import { useState } from 'react'
 
-export default function Test2() {
-  const state = useSyncExternalStore(
-    () => {
-      console.log('add')
-      return () => {
-        console.log('remove')
+// https://github.com/onesine/react-tailwindcss-select/blob/master/src/components/Select.tsx
+interface Option {
+  value: string
+  label: string
+  disabled?: boolean
+}
+
+type SelectProps = {
+  isDisabled?: boolean
+  options?: Option[]
+  value?: Option
+  onChange: (value: Option | null) => void
+}
+
+const Select: FC<SelectProps> = ({
+  isDisabled = false,
+  options = [],
+  value = null,
+  onChange,
+}) => {
+  const [open, setOpen] = useState(false)
+  const [inputValue, setInputValue] = useState('')
+  const ref = useRef<HTMLDivElement>(null)
+  const list = options.filter((option) =>
+    'disabled' in option ? !option.disabled : true,
+  )
+
+  const toggle = useCallback(() => {
+    if (!isDisabled) {
+      setOpen((prev) => !prev)
+    }
+  }, [isDisabled])
+
+  const closeDropDown = useCallback(() => {
+    setOpen(false)
+  }, [])
+
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        closeDropDown()
+      }
+    }
+
+    document.addEventListener('mousedown', listener)
+    document.addEventListener('touchstart', listener)
+
+    return () => {
+      document.removeEventListener('mousedown', listener)
+      document.removeEventListener('touchstart', listener)
+    }
+  }, [closeDropDown])
+
+  const onPressEnterOrSpace = useCallback(
+    (e: React.KeyboardEvent) => {
+      if (e.key === 'Enter' || e.key === ' ') {
+        e.preventDefault()
+        toggle()
       }
     },
-    () => {
-      console.log('client getSnapshot')
-      return 'client'
-    },
-    () => {
-      console.log('server getSnapshot')
-      return 'server'
-    },
+    [toggle],
   )
-  console.log(state)
-  return <div>{state}</div>
+
+  const handleValueChange = useCallback(
+    (selected: Option) => {
+      onChange(selected)
+      closeDropDown()
+    },
+    [closeDropDown],
+  )
+
+  const clearValue = useCallback(
+    (e: React.MouseEvent) => {
+      e.stopPropagation()
+      onChange(null)
+    },
+    [onChange],
+  )
+
+  return (
+    <div className="relative w-full">
+      <div aria-expanded="true" aria-controls="ttt">
+        <div>cuitao</div>
+      </div>
+      <div id="ttt">
+        <div>ctk</div>
+      </div>
+    </div>
+  )
+}
+
+export default function Test2() {
+  return <Select onChange={() => {}} />
 }
