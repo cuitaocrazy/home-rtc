3195d5aa82bbdf2ac9ad065a6f9644aeb6d2c1f9
cuitao <ctllf@gmail.com>
2022-11-09 23:04:47 +0800

    add select box

---
+++ app/hook/useSelect.tsx
@@ -0,0 +1,191 @@
+import { useCallback, useEffect, useRef, useState } from 'react'
+
+import useOnClickOutside from '~/hook/useOnClickOutside'
+import { useMyLayoutEffect } from '~/routes/test2'
+
+export function useSelect<T>() {
+  const [isOpen, setIsOpen] = useState(false)
+  const [selected, setSelected] = useState<T | null>(null)
+  const [scrollTop, setScrollTop] = useState(0)
+  const rootRef = useRef<HTMLElement | null>(null)
+  const inputRef = useRef<HTMLElement | null>(null)
+  const itemRefs = useRef<Map<number, HTMLElement>>(new Map())
+  const itemIndexMapRef = useRef<Map<T, number>>(new Map())
+  const indexItemMapRef = useRef<Map<number, T>>(new Map())
+  const menuRef = useRef<HTMLElement | null>(null)
+
+  const toggle = useCallback(() => {
+    if (menuRef.current) {
+      setScrollTop(menuRef.current.scrollTop)
+    }
+    setIsOpen((prev) => !prev)
+  }, [])
+
+  useMyLayoutEffect(() => {
+    if (isOpen && menuRef.current) {
+      menuRef.current.scrollTop = scrollTop
+    }
+  }, [isOpen])
+
+  const itemRefHandler = useCallback(
+    (idx: number, item: T) => (ref: HTMLElement | null) => {
+      if (ref) {
+        itemRefs.current.set(idx, ref)
+        itemIndexMapRef.current.set(item, idx)
+        indexItemMapRef.current.set(idx, item)
+
+        const clickHandler = () => {
+          setSelected(item)
+          toggle()
+        }
+
+        ref.addEventListener('click', clickHandler)
+      } else {
+        itemRefs.current.delete(idx)
+        itemIndexMapRef.current.delete(item)
+        indexItemMapRef.current.delete(idx)
+      }
+    },
+    [toggle],
+  )
+
+  const menuRefHandler = useCallback((ref: HTMLElement | null) => {
+    menuRef.current = ref
+
+    if (ref) {
+      ref.addEventListener('mousedown', (e) => e.preventDefault())
+    }
+  }, [])
+
+  const rootRefHandler = useCallback((ref: HTMLElement | null) => {
+    rootRef.current = ref
+  }, [])
+
+  const inputRefHandler = useCallback((ref: HTMLElement | null) => {
+    inputRef.current = ref
+  }, [])
+
+  const outsideClickHandler = useCallback(
+    (_: MouseEvent | TouchEvent) => {
+      if (isOpen) {
+        toggle()
+      }
+    },
+    [isOpen, toggle],
+  )
+
+  useOnClickOutside(rootRef, outsideClickHandler)
+
+  useEffect(() => {
+    if (!inputRef.current) {
+      return
+    }
+
+    const input = inputRef.current
+
+    const clickHandler = (e: MouseEvent) => {
+      toggle()
+    }
+
+    input.addEventListener('click', clickHandler)
+
+    return () => {
+      input.removeEventListener('click', clickHandler)
+    }
+  }, [toggle])
+
+  useEffect(() => {
+    if (!inputRef.current) {
+      return
+    }
+
+    const input = inputRef.current
+
+    const keydownHandler = (e: KeyboardEvent) => {
+      if (e.key === 'Enter' || e.key === ' ') {
+        toggle()
+      }
+
+      if (e.key === 'Escape' && isOpen) {
+        toggle()
+      }
+
+      if (e.key === 'ArrowDown') {
+        e.preventDefault()
+        if (selected) {
+          const idx = itemIndexMapRef.current.get(selected)!
+          const next = indexItemMapRef.current.get(idx + 1)
+
+          if (next) {
+            setSelected(next)
+          } else {
+            setSelected(indexItemMapRef.current.get(0)!)
+          }
+        } else {
+          setSelected(indexItemMapRef.current.get(0)!)
+        }
+      }
+
+      if (e.key === 'ArrowUp') {
+        e.preventDefault()
+
+        if (selected) {
+          const idx = itemIndexMapRef.current.get(selected)!
+          const next = indexItemMapRef.current.get(idx - 1)
+
+          if (next) {
+            setSelected(next)
+          } else {
+            setSelected(
+              indexItemMapRef.current.get(indexItemMapRef.current.size - 1)!,
+            )
+          }
+        } else {
+          setSelected(
+            indexItemMapRef.current.get(indexItemMapRef.current.size - 1)!,
+          )
+        }
+      }
+    }
+
+    input.addEventListener('keydown', keydownHandler)
+
+    return () => {
+      input.removeEventListener('keydown', keydownHandler)
+    }
+  })
+
+  useMyLayoutEffect(() => {
+    if (!selected || !menuRef.current) return
+
+    const menuEl = menuRef.current
+    const targetEl = itemRefs.current.get(
+      itemIndexMapRef.current.get(selected)!,
+    )!
+
+    const { top: boundingTop, bottom: boundingBottom } =
+      menuEl.getBoundingClientRect()
+    const { top: targetTop, bottom: targetBottom } =
+      targetEl.getBoundingClientRect()
+    const styles = window.getComputedStyle(menuEl)
+    const borderTop = parseInt(styles.borderTopWidth, 10)
+    const borderBottom = parseInt(styles.borderBottomWidth, 10)
+
+    if (targetTop < boundingTop + borderTop) {
+      menuEl.scrollTop -= boundingTop - targetTop + borderTop
+    } else if (targetBottom > boundingBottom - borderBottom) {
+      menuEl.scrollTop += targetBottom - boundingBottom + borderBottom
+    }
+  }, [selected])
+
+  return {
+    isOpen,
+    selected,
+    toggle,
+    setSelected,
+    rootRefHandler,
+    itemRefHandler,
+    menuRefHandler,
+    inputRefHandler,
+  }
+}
--- app/routes/test2.tsx
+++ app/routes/test2.tsx
@@ -2,192 +2,23 @@ import { faker } from '@faker-js/faker'
 import { FiChevronDown } from '@react-icons/all-files/fi/FiChevronDown'
 import { FiChevronUp } from '@react-icons/all-files/fi/FiChevronUp'
 import clsx from 'clsx'
-import {
-  useCallback,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from 'react'
+import { useEffect, useLayoutEffect, useState } from 'react'
 
-import useOnClickOutside from '~/hook/useOnClickOutside'
+import { useSelect } from '../hook/useSelect'
 
-const useMyLayoutEffect: typeof useLayoutEffect =
+export const useMyLayoutEffect: typeof useLayoutEffect =
   typeof window !== 'undefined' ? useLayoutEffect : useEffect
-
-function useSelect<T>(
-  getNext: (current: T | null, direction: number) => [string, T],
-) {
-  const [isOpen, setIsOpen] = useState(false)
-  const [selected, setSelected] = useState<[string, T] | null>(null)
-  const [scrollTop, setScrollTop] = useState(0)
-  const rootRef = useRef<HTMLElement | null>(null)
-  const inputRef = useRef<HTMLElement | null>(null)
-  const itemRefs = useRef<{ [key: string]: HTMLElement }>({})
-  const menuRef = useRef<HTMLElement | null>(null)
-
-  const toggle = useCallback(() => {
-    if (menuRef.current) {
-      setScrollTop(menuRef.current.scrollTop)
-    }
-    setIsOpen((prev) => !prev)
-  }, [])
-
-  useMyLayoutEffect(() => {
-    if (isOpen && menuRef.current) {
-      menuRef.current.scrollTop = scrollTop
-    }
-  }, [isOpen])
-
-  const itemRefHandler = useCallback((key: string, ref: HTMLElement | null) => {
-    if (ref) {
-      itemRefs.current[key] = ref
-    } else {
-      delete itemRefs.current[key]
-    }
-  }, [])
-
-  const menuRefHandler = useCallback((ref: HTMLElement | null) => {
-    menuRef.current = ref
-  }, [])
-
-  const rootRefHandler = useCallback((ref: HTMLElement | null) => {
-    rootRef.current = ref
-  }, [])
-
-  const inputRefHandler = useCallback((ref: HTMLElement | null) => {
-    inputRef.current = ref
-  }, [])
-
-  const outsideClickHandler = useCallback(
-    (_: MouseEvent | TouchEvent) => {
-      if (isOpen) {
-        toggle()
-      }
-    },
-    [isOpen, toggle],
-  )
-
-  useOnClickOutside(rootRef, outsideClickHandler)
-
-  useEffect(() => {
-    if (!inputRef.current) {
-      return
-    }
-
-    const input = inputRef.current
-
-    const clickHandler = (e: MouseEvent) => {
-      toggle()
-    }
-
-    input.addEventListener('click', clickHandler)
-
-    return () => {
-      input.removeEventListener('click', clickHandler)
-    }
-  }, [toggle])
-
-  useEffect(() => {
-    if (!inputRef.current) {
-      return
-    }
-
-    const input = inputRef.current
-
-    const keydownHandler = (e: KeyboardEvent) => {
-      if (e.key === 'Enter' || e.key === ' ') {
-        toggle()
-      }
-
-      if (e.key === 'Escape' && isOpen) {
-        toggle()
-      }
-
-      if (e.key === 'ArrowDown') {
-        e.preventDefault()
-        setSelected(getNext(selected?.[1] || null, 0))
-      }
-
-      if (e.key === 'ArrowUp') {
-        e.preventDefault()
-        setSelected(getNext(selected?.[1] || null, 1))
-      }
-    }
-
-    input.addEventListener('keydown', keydownHandler)
-
-    return () => {
-      input.removeEventListener('keydown', keydownHandler)
-    }
-  })
-
-  useMyLayoutEffect(() => {
-    if (!selected?.[0] || !menuRef.current) return
-
-    const menuEl = menuRef.current
-    const targetEl = itemRefs.current[selected[0]]
-
-    const { top: boundingTop, bottom: boundingBottom } =
-      menuEl.getBoundingClientRect()
-    const { top: targetTop, bottom: targetBottom } =
-      targetEl.getBoundingClientRect()
-    const styles = window.getComputedStyle(menuEl)
-    const borderTop = parseInt(styles.borderTopWidth, 10)
-    const borderBottom = parseInt(styles.borderBottomWidth, 10)
-
-    if (targetTop < boundingTop + borderTop) {
-      menuEl.scrollTop -= boundingTop - targetTop + borderTop
-    } else if (targetBottom > boundingBottom - borderBottom) {
-      menuEl.scrollTop += targetBottom - boundingBottom + borderBottom
-    }
-  }, [selected])
-
-  return {
-    isOpen,
-    selected,
-    toggle,
-    setSelected,
-    rootRefHandler,
-    itemRefHandler,
-    menuRefHandler,
-    inputRefHandler,
-  }
-}
 
 function Test2() {
   const [data, setData] = useState<{ id: number; name: string }[]>([])
   const {
     isOpen,
-    toggle,
     selected,
-    setSelected,
     rootRefHandler,
     itemRefHandler,
     menuRefHandler,
     inputRefHandler,
-  } = useSelect<typeof data[number]>((current, direction) => {
-    let next: typeof data[number] | undefined = undefined
-    if (direction === 0) {
-      if (!current) {
-        next = data[0]
-      } else {
-        const idx = data.findIndex((d) => d.id === current.id)
-        const nextIdx = idx + 1
-        next = data[nextIdx] || data[0]
-      }
-    } else {
-      if (!current) {
-        next = data[data.length - 1]
-      } else {
-        const idx = data.findIndex((d) => d.id === current.id)
-        const nextIdx = idx - 1
-        next = data[nextIdx] || data[data.length - 1]
-      }
-    }
-
-    return [next.id.toString(), next]
-  })
+  } = useSelect<typeof data[number]>()
 
   useEffect(() => {
     if (data.length === 0) {
@@ -201,15 +32,15 @@ function Test2() {
   }, [data])
 
   return (
-    <div className="relative m-1 w-56" ref={rootRefHandler}>
+    <div className="relative w-full" ref={rootRefHandler}>
       <div className="relative">
         <input
           ref={inputRefHandler}
           type="text"
           placeholder="select"
-          value={selected?.[1].name ?? ''}
+          value={selected?.name ?? ''}
           readOnly
-          className="w-full border py-2 pl-2 pr-6 focus:outline-none"
+          className="w-full py-2 pl-2 pr-6"
         />
         <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
           {isOpen ? <FiChevronUp /> : <FiChevronDown />}
@@ -217,19 +48,16 @@ function Test2() {
       </div>
       {isOpen && (
         <ul
-          className="bg-gray-300 w-full max-h-32 overflow-y-auto pl-2"
-          onMouseDown={(e) => e.preventDefault()}
+          className="bg-gray-300 absolute w-full max-h-32 overflow-y-auto"
           ref={menuRefHandler}
         >
           {data.map((item) => (
             <li
-              ref={(el) => itemRefHandler(`${item.id}`, el)}
+              ref={itemRefHandler(item.id, item)}
               key={item.id}
-              className={clsx({ 'bg-gray-700': selected?.[1].id === item.id })}
-              onClick={(e) => {
-                setSelected([`${item.id}`, item])
-                toggle()
-              }}
+              className={clsx('pl-2', {
+                'bg-gray-700': selected?.id === item.id,
+              })}
             >
               {item.name}
             </li>
