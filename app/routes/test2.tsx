a23bd3bbe08f194c3a847a59bc367b862f2359f5
cuitao <ctllf@gmail.com>
2022-11-10 21:45:43 +0800

    add motion test

--- app/routes/test2.tsx
+++ app/routes/test2.tsx
@@ -2,6 +2,7 @@ import { faker } from '@faker-js/faker'
 import { FiChevronDown } from '@react-icons/all-files/fi/FiChevronDown'
 import { FiChevronUp } from '@react-icons/all-files/fi/FiChevronUp'
 import clsx from 'clsx'
+import { AnimatePresence, motion } from 'framer-motion'
 import { useEffect, useLayoutEffect, useState } from 'react'
 
 import { useSelect } from '../hook/useSelect'
@@ -40,30 +41,56 @@ function Test2() {
           placeholder="select"
           value={selected?.name ?? ''}
           readOnly
-          className="w-full py-2 pl-2 pr-6"
+          className="w-full py-2 pl-2 pr-6 focus:outline-none"
         />
         <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
           {isOpen ? <FiChevronUp /> : <FiChevronDown />}
         </span>
       </div>
-      {isOpen && (
+      <AnimatePresence>
+        {isOpen && (
+          <motion.ul
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="bg-gray-300 absolute w-full max-h-32 overflow-y-auto animate-spin-slow"
+            ref={menuRefHandler}
+          >
+            {data.map((item) => (
+              <motion.li
+                ref={itemRefHandler(item.id, item)}
+                key={item.id}
+                className={clsx('pl-2 hover:bg-gray-600 transition-all', {
+                  'bg-gray-700': selected?.id === item.id,
+                })}
+              >
+                {item.name}
+              </motion.li>
+            ))}
+          </motion.ul>
+        )}
+      </AnimatePresence>
+      {/* {isOpen && (
         <ul
-          className="bg-gray-300 absolute w-full max-h-32 overflow-y-auto"
+          className="bg-gray-300 absolute w-full max-h-32 overflow-y-auto animate-spin-slow"
           ref={menuRefHandler}
         >
           {data.map((item) => (
             <li
               ref={itemRefHandler(item.id, item)}
               key={item.id}
-              className={clsx('pl-2', {
-                'bg-gray-700': selected?.id === item.id,
-              })}
+              className={clsx(
+                'pl-2 hover:bg-gray-600 transition-all duration-300',
+                {
+                  'bg-gray-700': selected?.id === item.id,
+                },
+              )}
             >
               {item.name}
             </li>
           ))}
         </ul>
-      )}
+      )} */}
     </div>
   )
 }
--- app/routes/test3.tsx
+++ app/routes/test3.tsx
@@ -1,115 +1,19 @@
-import { useEffect, useRef } from 'react'
+import { motion } from 'framer-motion'
 
-function useSelectEvent(
-  // inputRef: React.RefObject<HTMLElement>,
-  optionRef: React.RefObject<HTMLElement>,
-) {
-  useEffect(() => {
-    if (optionRef.current) {
-      const clickHandler = (e: MouseEvent) => {
-        console.log('click3', e.eventPhase, performance.now())
-      }
-      const ele = optionRef.current
-      ele.addEventListener('click', clickHandler)
-
-      return () => {
-        ele.removeEventListener('click', clickHandler)
-      }
-    }
-  }, [optionRef])
-}
-function Test3() {
-  const click1Ref = useRef<HTMLDivElement>(null)
-  const click2Ref = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const clickHandler = (e: MouseEvent): void => {
-      console.log('window click', e.eventPhase, performance.now())
-    }
-    window.addEventListener('click', clickHandler)
-
-    return () => {
-      window.removeEventListener('click', clickHandler)
-    }
-  }, [])
-
-  useEffect(() => {
-    const clickHandler = (e: MouseEvent): void => {
-      console.log('document click', e.eventPhase, performance.now())
-    }
-    document.addEventListener('click', clickHandler)
-
-    return () => {
-      document.removeEventListener('click', clickHandler)
-    }
-  }, [])
-
-  useEffect(() => {
-    const clickHandler = (e: MouseEvent): void => {
-      console.log(
-        'document.documentElement click',
-        e.eventPhase,
-        performance.now(),
-      )
-    }
-    document.documentElement.addEventListener('click', clickHandler)
-
-    return () => {
-      document.documentElement.removeEventListener('click', clickHandler)
-    }
-  }, [])
-
-  useEffect(() => {
-    if (click1Ref.current) {
-      const clickHandler = (e: MouseEvent) => {
-        console.log('click1', e.eventPhase, performance.now())
-        console.log(e.target, e.currentTarget)
-      }
-      const ele = click1Ref.current
-      ele.addEventListener('click', clickHandler)
-      ele.addEventListener('click', clickHandler, true)
-
-      return () => {
-        ele.removeEventListener('click', clickHandler)
-        ele.removeEventListener('click', clickHandler, true)
-      }
-    }
-  }, [])
-
-  useEffect(() => {
-    if (click2Ref.current) {
-      const clickHandler = (e: MouseEvent) => {
-        console.log('click2', e.eventPhase, performance.now())
-      }
-      const ele = click2Ref.current
-      ele.addEventListener('click', clickHandler)
-
-      return () => {
-        ele.removeEventListener('click', clickHandler)
-      }
-    }
-  }, [])
-  useSelectEvent(click1Ref)
+const Test3 = () => {
   return (
-    <div>
-      <div
-        ref={click1Ref}
-        onClick={(e) =>
-          console.log('react click1', e.eventPhase, performance.now())
-        }
-      >
-        click1
-        <div
-          ref={click2Ref}
-          onClick={(e) => {
-            console.log('react click2', e.eventPhase, performance.now())
-            console.log(e.target, e.currentTarget)
-          }}
-        >
-          click2
-        </div>
-      </div>
-    </div>
+    <motion.div
+      initial={{ opacity: 1, scale: 0.5 }}
+      animate={{
+        scale: [1, 2, 2, 1, 1],
+        rotate: [0, 0, 270, 270, 0],
+        borderRadius: ['20%', '20%', '50%', '50%', '20%'],
+      }}
+      // exit={{ opacity: 0 }}
+      // transition={{ duration: 1 }}
+    >
+      <h1>Test3</h1>
+    </motion.div>
   )
 }
 
